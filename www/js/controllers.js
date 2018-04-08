1d1ef0ef4248e7e1b6f29e250ac43d4151bf1fe6
Lucas Santos <lucas.moreira@me.com>
2018-04-08 03:40:04 -0300

    Adding maps

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,51 @@
 angular.module('starter.controllers', [])
 
 .controller('EventsCtrl', function($scope, $ionicModal) {
-  $scope.openModal = function() {
-    $ionicModal.fromTemplateUrl('templates/event-view.html', {
-      scope: $scope
-    }).then(function(modal) {
-      $scope.modal = modal;
-      $scope.modal.show();
-    });
-  }
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 13,
+            center: {lat: -23.5616153, lng: -46.6849476},
+            gestureHandling: "greedy",
+            zoomControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            streetViewControl: false,
+            rotateControl: false,
+            fullscreenControl: false
+        });
+
+        var features = [{
+            position: new google.maps.LatLng(-23.5632054, -46.656439),
+            type: 'info'
+        }, {
+            position: new google.maps.LatLng(-23.5569532, -46.6611564),
+            type: 'info'
+        }, {
+            position: new google.maps.LatLng(-23.5597883, -46.7303938),
+            type: 'info'
+        }];
+
+        var markers = features.map(function(feature) {
+            return new google.maps.Marker({
+              position: feature.position,
+              icon: 'https://s18.postimg.org/dn3av6ovt/lmss32.png',
+              map: map
+            });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        // var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    }
+
+    $scope.openModal = function() {
+        $ionicModal.fromTemplateUrl('templates/event-view.html', {
+          scope: $scope
+        }).then(function(modal) {
+          $scope.modal = modal;
+          $scope.modal.show();
+          initMap();
+        });
+    }
 })
 
 .controller('EventViewCtrl', function($scope, $ionicModal) {
