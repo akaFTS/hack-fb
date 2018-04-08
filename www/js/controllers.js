66e79e67084ee55812a687c2086c08ee83318057
Gustavo Silva <gustavo@cupertino.local>
2018-04-08 00:19:32 -0300

    lots of stuff

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,22 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('EventsCtrl', function($scope, $ionicModal) {
+  $scope.openModal = function() {
+    $ionicModal.fromTemplateUrl('templates/event-view.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  }
+})
+
+.controller('EventViewCtrl', function($scope, $ionicModal) {
+  
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('FeedCtrl', function($scope) {
 });
